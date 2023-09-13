17ce96f1c6b690c854892d1d5973ada2e547777a
Maksim Karlo <mkarlo@scorewarrior.com>
2023-09-13 11:26:53 +0300

    main window randomizer function - gets random movie from kinopoisk api

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import ttk
+from PIL import ImageTk, Image
+from utils.randomizer import get_random_movie
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from async_tkinter_loop import async_mainloop
 
+# main window
+root = tk.Tk()
+root.geometry("250x380")
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# dice image
+image = Image.open("assets/dice.png")
+new_size = (210, 150)
+resized_image = image.resize(new_size, Image.ANTIALIAS)
+dice_image = ImageTk.PhotoImage(resized_image)
 
+# image label
+image = ttk.Label(root, image=dice_image)
+image.grid(row=0, column=0, columnspan=1, rowspan=1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# text label
+label = ttk.Label(root, text="Get random movie!", wraplength=200, justify="center")
+label.grid(row=1, column=0, columnspan=1, rowspan=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# random movie button
+button = ttk.Button(root, text="Go",
+                    command=lambda: [get_random_movie(image, label, button)])
+button.grid(row=2, column=0, columnspan=1, rowspan=1)
+
+# exit button
+exit_button = ttk.Button(root, text="Exit", command=root.destroy)
+exit_button.grid(row=3, column=0, columnspan=1, rowspan=1)
+
+async_mainloop(root)
---
+++ utils/randomizer.py
@@ -0,0 +1,32 @@
+import httpx
+import requests
+from PIL import Image, ImageTk
+
+from async_tkinter_loop import async_handler
+
+
+@async_handler
+async def get_random_movie(img, lbl, btn):
+    btn.config(state="disabled")
+    btn.config(text="Loading...")
+    async with httpx.AsyncClient() as client:
+        url = "https://api.kinopoisk.dev/v1.3/movie/random"
+        headers = {
+            "X-API-KEY": "SPX4QTH-PNY4E5J-MZR2RHR-1XNTB82"
+        }
+        response = await client.get(url, headers=headers)
+        poster_url = str(response.json()["poster"]['url'])
+        image_data = Image.open(requests.get(poster_url, stream=True).raw)
+
+        image_data = image_data.resize((200, 300), Image.ANTIALIAS)
+
+        # Convert the PIL Image to a PhotoImage for tkinter
+        global tk_image  # Keep a global reference to tk_image
+        tk_image = ImageTk.PhotoImage(image_data)
+        img.config(image=tk_image)
+
+        lbl.config(
+            text=f'{response.json()["rating"]["imdb"]} ' + f'{response.json()["name"]}' + f' ({response.json()["year"]})')
+
+    btn.config(state="normal")
+    btn.config(text="Go")
